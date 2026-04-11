8bde4466c137a07a45dbbfcce6b56fed795ffb47
lianni <liadev2006@gmail.com>
2026-04-11 11:45:37 -0400

    Configurar ESLint para cambios en código

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,19 +1,19 @@
 import { Route, Routes } from 'react-router-dom'
-import { Suspense , lazy } from 'react'
+import { Suspense, lazy } from 'react'
 import LazyLoading from './components/LazyLoading'
 
 const LoginPage = lazy(() => import('../pages/LoginPage'))
 
-const AppRoutes=()=>{
+const AppRoutes = () => {
 
-  return(
-    <Suspense fallback={<LazyLoading/>}>
+  return (
+    <Suspense fallback={<LazyLoading />}>
       <Routes>
 
-      <Route path='/login' element={<LoginPage/>} />
-      <Route path='*' element={<LoginPage/>}></Route>
-  
-     </Routes>
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='*' element={<LoginPage />}></Route>
+
+      </Routes>
     </Suspense>
   )
 }
