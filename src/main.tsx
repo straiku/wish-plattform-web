4a7f0e9dc606489315a36226f78bdbdde7a7d542
YarielZC <yarielzamoracueto@gmail.com>
2026-04-22 20:22:36 -0400

    core: update heroui lib to v3.0.3

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { HeroUIProvider } from '@heroui/react'
 import { Toaster } from 'sileo'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HeroUIProvider>
-      <Toaster>
-        <BrowserRouter>
+    <Toaster>
+      <BrowserRouter>
         <App />
-        </BrowserRouter>
-      </Toaster>
-    </HeroUIProvider>
+      </BrowserRouter>
+    </Toaster>
   </StrictMode>
 )
