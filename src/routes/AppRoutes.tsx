066a65ae61d952f47b59661254c50fd5e4c480d9
YarielZC <yarielzamoracueto@gmail.com>
2026-04-30 22:08:34 -0400

    feat(Layout): create layout for landing page and add it to app routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import { Suspense, lazy } from 'react'
 import LazyLoading from './components/LazyLoading'
+import LandingPageLayout from '../layouts/LandingPageLayout/LandingPageLayout'
 
 const LoginPage = lazy(() => import('../pages/LoginPage/LoginPage'))
 const LandingPage = lazy(() => import('../pages/LandingPage/LandingPage'))
@@ -10,7 +11,9 @@ const AppRoutes = () => {
     <Suspense fallback={<LazyLoading />}>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<LandingPageLayout />}>
+          <Route index element={<LandingPage />} />
+        </Route>
         <Route path="*" element={<LoginPage />}></Route>
       </Routes>
     </Suspense>
