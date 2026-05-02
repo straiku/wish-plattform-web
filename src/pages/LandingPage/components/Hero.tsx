c1406f2757d73aa33fbdfaf052c9710755a8fc39
JustEMAKILLER <emanuelnuviola@gmail.com>
2026-05-02 00:24:03 -0400

    style(hero): responsive

--- src/pages/LandingPage/components/Hero.tsx
+++ src/pages/LandingPage/components/Hero.tsx
@@ -7,21 +7,21 @@ export default function Hero() {
         <p className="font-primary text-neutral text-sm tracking-wider">
           EQUIPO DE WISHFACTORY NO. 1
         </p>
-        <p className="font-secondary w-2/4 text-8xl">
+        <p className="font-secondary w-2/4 text-8xl max-[530px]:text-5xl">
           El Futuro se Construye con <span className="text-amber-800 italic">Intención</span>.
         </p>
-        <div className="font-primary mt-3 text-sm">
-          <p className="text-neutral w-lg text-lg">
+        <div className="font-primary mt-3">
+          <p className="text-neutral w-lg text-lg max-[530px]:w-auto">
             En el corazón de Wish Factory, cada palabra es un átomo.
             <br />
             Forjamos desde realidades digitales a través de la voluntad y el diseño abosluto.
           </p>
         </div>
         <div className="font-secondary mt-7 flex gap-6">
-          <Button size="lg" className="text-primary rounded-lg bg-black">
+          <Button size="lg" className="text-primary rounded-lg bg-black max-[530px]:max-w-min">
             INICIAR
           </Button>
-          <Button size="lg" variant="outline" className="rounded-lg">
+          <Button size="lg" variant="outline" className="rounded-lg max-[530px]:max-w-min">
             EXPLORAR WISHES
           </Button>
         </div>
