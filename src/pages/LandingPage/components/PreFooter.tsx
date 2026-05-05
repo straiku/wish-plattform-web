d41a8d59d76ed54b9a3c9f8df3458c58cc02af63
JustEMAKILLER <emanuelnuviola@gmail.com>
2026-05-05 16:08:38 -0400

    refactor(PreFooter): minor span fix

--- src/pages/LandingPage/components/PreFooter.tsx
+++ src/pages/LandingPage/components/PreFooter.tsx
@@ -6,7 +6,7 @@ export default function PreFooter() {
       <div className="font-secondary flex flex-col flex-wrap items-center gap-4 text-8xl max-[530px]:text-5xl">
         <p>
           ¿Estás listo para forjar tu
-          <span className="decoration-secondary underline"> propia realidad?</span>
+          <span className="decoration-secondary underline"> propia realidad</span>?
         </p>
         <p className="font-primary text-sm">
           La entrada a WishFactory no es para todos. Solo para quienes entienden que soñar es el
