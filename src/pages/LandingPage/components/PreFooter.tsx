6c81f1100a3283d892a4d183b5a5275687c7f79a
JustEMAKILLER <emanuelnuviola@gmail.com>
2026-05-05 23:39:35 -0400

    style(prefooter): fix underline space

--- src/pages/LandingPage/components/PreFooter.tsx
+++ src/pages/LandingPage/components/PreFooter.tsx
@@ -2,11 +2,11 @@ import { Button } from '@heroui/react'
 
 export default function PreFooter() {
   return (
-    <section className="mx-auto w-full text-center">
+    <section className="mx-auto w-full max-w-7xl text-center">
       <div className="font-secondary flex flex-col flex-wrap items-center gap-4 text-8xl max-[530px]:text-5xl">
         <p>
-          ¿Estás listo para forjar tu
-          <span className="decoration-secondary underline"> propia realidad</span>?
+          ¿Estás listo para forjar tu&nbsp;
+          <span className="decoration-secondary underline">propia realidad</span>?
         </p>
         <p className="font-primary text-sm">
           La entrada a WishFactory no es para todos. Solo para quienes entienden que soñar es el
