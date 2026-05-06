4017f9262405d95b0207b7833d75a55bcc3ee7c1
YarielZC <yarielzamoracueto@gmail.com>
2026-05-06 03:27:34 -0400

    refactor(prefooter): refactor size of font in responsive styles

--- src/pages/LandingPage/components/PreFooter.tsx
+++ src/pages/LandingPage/components/PreFooter.tsx
@@ -2,13 +2,13 @@ import { Button } from '@heroui/react'
 
 export default function PreFooter() {
   return (
-    <section className="mx-auto w-full max-w-7xl text-center">
-      <div className="font-secondary flex flex-col flex-wrap items-center gap-4 text-8xl max-[530px]:text-5xl">
-        <p>
+    <section className="mx-auto w-full py-20 text-center">
+      <div className="font-secondary flex max-w-7xl flex-col flex-wrap items-center gap-4 px-6 max-[870px]:p-0">
+        <p className="w-8/12 text-6xl max-[870px]:text-5xl max-[675px]:w-10/12 max-[530px]:w-full max-[530px]:text-4xl">
           ¿Estás listo para forjar tu&nbsp;
-          <span className="decoration-secondary underline">propia realidad</span>?
+          <span className="decoration-secondary italic underline">propia realidad</span>?
         </p>
-        <p className="font-primary text-sm">
+        <p className="font-primary w-10/12 text-sm max-[530px]:text-xs">
           La entrada a WishFactory no es para todos. Solo para quienes entienden que soñar es el
           primer paso para crear.
         </p>
