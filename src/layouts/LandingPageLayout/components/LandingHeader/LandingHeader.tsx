75917e840fd1fcb3ab52f27f070dfee34a0eeeb9
YarielZC <yarielzamoracueto@gmail.com>
2026-05-06 02:22:39 -0400

    fix(LandingHeader): fix z-index of the header

--- src/layouts/LandingPageLayout/components/LandingHeader/LandingHeader.tsx
+++ src/layouts/LandingPageLayout/components/LandingHeader/LandingHeader.tsx
@@ -18,7 +18,7 @@ const LandingHeader = () => {
 
   return (
     <div>
-      <header className="bg-primary fixed inset-x-0 mx-auto flex h-16 w-full max-w-[1920px] flex-col max-[850px]:justify-between">
+      <header className="bg-primary fixed inset-x-0 z-50 mx-auto flex h-16 w-full max-w-[1920px] flex-col max-[850px]:justify-between">
         <div className="grid h-full grid-cols-3 items-center max-[850px]:grid-cols-2">
           <h1 className="font-secondary ml-14 text-2xl italic max-[850px]:-mb-2 max-[850px]:ml-6 max-[850px]:text-lg">
             WishFactory
