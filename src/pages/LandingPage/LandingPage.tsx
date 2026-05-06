cecb9fb40a28f5b21004e1bd43bdd4b9aedc0445
YarielZC <yarielzamoracueto@gmail.com>
2026-05-06 02:55:44 -0400

    feat(LandingPage): add MixWish section into Landing Page

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,10 +1,12 @@
 import Hero from './components/Hero'
+import MixWish from './components/MixWish'
 import PreFooter from './components/PreFooter'
 
 export default function LandingPage() {
   return (
     <div className="bg-primary min-h-screen">
       <Hero />
+      <MixWish />
       <PreFooter />
     </div>
   )
