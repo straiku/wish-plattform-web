7368bc0c73e046f3fab192bef483e5ad1a5057c5
lianni <liadev2006@gmail.com>
2026-05-07 07:37:02 -0500

    feat(landing) : add preview section?

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,9 +1,15 @@
 import Hero from './components/Hero'
+import Preview from './components/Preview'
 
 export default function LandingPage() {
   return (
-    <div className="bg-primary">
-      <Hero />
+    <div>
+      <div className="bg-primary">
+        <Hero/>
+      </div>
+      <div className="bg-white">
+          <Preview/>
+      </div> 
     </div>
   )
 }
---
+++ src/pages/LandingPage/components/Preview.tsx
@@ -0,0 +1,34 @@
+
+
+export default function Preview() {
+  return (
+    <section className=" min-h-screen  max-w-7xl py-44 max-[1280px]:mx-6 min-[1280px]:mx-auto">
+      <div className="font-primary text-neutral tracking-[0.3rem] mb-25 text-xs flex ">
+       FRAGMENTO DE WISH NO.36
+      </div>
+
+      <div className="flex flex-nowrap  justify-center gap-5 items-strech w-full text-7xl max-[530px]:text-5xl  text-left " >
+        <div className="w-0.5  bg-gray-200 " />   
+        <div className="font-secondary flex flex-col">
+          <p>Deseo una</p> 
+          <p>ciudad donde la</p>
+          <p className="italic" >arquitectura</p> 
+          <p>
+            <span className='italic'>respire</span>
+            ,donde
+          </p> 
+          <p>los muros se</p> 
+          <p>plieguen al</p>
+          <p>ritmo del viento</p> 
+          <p>y el hormigón</p> 
+          <p>aprenda a soñar</p> 
+          <p>con el musgo."</p> 
+        </div>
+      </div>
+      
+      <div className="font-secondary italic text-neutral mt-20 ml-[70%] flex gap-6">
+        El observador
+      </div>
+    </section>
+  )
+}
