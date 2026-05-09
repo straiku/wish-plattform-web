c56d4979d74f6e2019aa96838b032f64b21e78cc
YarielZC <yarielzamoracueto@gmail.com>
2026-05-09 16:53:28 -0400

    fix(Preview): limit the max witdh into preview section

--- src/pages/LandingPage/components/Preview.tsx
+++ src/pages/LandingPage/components/Preview.tsx
@@ -1,32 +1,30 @@
-
-
 export default function Preview() {
   return (
-    <section className='bg-white mx-auto w-full py-44 '>
-      <div className='font-primary ml-15 text-neutral tracking-[0.3rem] mb-25 text-xs flex '>
-       FRAGMENTO DE WISH NO.36
-      </div>
+    <section className="mx-auto w-full bg-white py-44">
+      <p className="font-primary text-neutral mx-auto mb-25 flex max-w-7xl text-xs tracking-[0.3rem] max-[530px]:ml-6">
+        FRAGMENTO DE WISH NO.36
+      </p>
 
-      <div className='flex flex-nowrap  justify-center gap-5 items-strech w-full text-6xl max-[530px]:text-5xl  text-left ' >
-        <div className='w-0.5  bg-gray-200 ' />   
-        <div className='font-secondary flex flex-col'>
-          <p>Deseo una</p> 
+      <div className="items-strech flex w-full flex-nowrap justify-center gap-5 text-left text-6xl max-[530px]:text-5xl">
+        <div className="w-0.5 bg-gray-200" />
+        <div className="font-secondary flex flex-col">
+          <p>Deseo una</p>
           <p>ciudad donde la</p>
-          <p className='italic' >arquitectura</p> 
+          <p className="italic">arquitectura</p>
           <p>
-            <span className='italic'>respire</span>
+            <span className="italic">respire</span>
             ,donde
-          </p> 
-          <p>los muros se</p> 
+          </p>
+          <p>los muros se</p>
           <p>plieguen al</p>
-          <p>ritmo del viento</p> 
-          <p>y el hormigón</p> 
-          <p>aprenda a soñar</p> 
-          <p>con el musgo.'</p> 
+          <p>ritmo del viento</p>
+          <p>y el hormigón</p>
+          <p>aprenda a soñar</p>
+          <p>con el musgo.'</p>
         </div>
       </div>
-      
-      <div className='font-secondary italic text-neutral mt-20 ml-[70%] flex gap-6'>
+
+      <div className="font-secondary text-neutral mt-20 ml-[70%] flex gap-6 italic">
         El observador
       </div>
     </section>
