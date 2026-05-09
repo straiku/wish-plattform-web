bf8f22c420b6c09e25bf8e246caf7247e06c7a6d
lianni <liadev2006@gmail.com>
2026-05-09 12:27:36 -0500

    feat/landingPage) : create section remixes

--- src/pages/LandingPage/components/Remixes/Remixes.tsx
+++ src/pages/LandingPage/components/Remixes/Remixes.tsx
@@ -1,8 +1,8 @@
 
 import HeroRemixes from './components/HeroRemixes'
 import HowItWorks from './components/HowItWorks'
-
 export default function Remixes(){
+
 
   return (
     <section className=' p-15 w-full bg-primary  h-auto flex justify-between flex-wrap max-[970px]:p-10'>
