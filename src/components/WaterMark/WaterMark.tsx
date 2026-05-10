5683d9d3cd0e946ac9a16361d1b1ed9e03f54af6
YarielZC <yarielzamoracueto@gmail.com>
2026-05-10 14:32:31 -0400

    refactor(WaterMark): structure to use the right color pallette

--- src/components/WaterMark/WaterMark.tsx
+++ src/components/WaterMark/WaterMark.tsx
@@ -1,8 +1,8 @@
 export default function WaterMark() {
   return (
-    <div className="font-primary text-neutral flex w-full justify-between px-10 py-10 text-xs tracking-wider">
-      <p>© 2026 WISHFACTORY</p>
-      <p>DESARROLLADO POR STRAiKU</p>
+    <div className="font-primary text-neutral mt-8 flex w-full justify-between border-t px-10 pt-4 text-xs tracking-wider max-[850px]:px-4">
+      <p className="max-[850px]:text-[0.5rem]">© 2026 WISHFACTORY</p>
+      <p className="text-right max-[850px]:text-[0.5rem]">DESARROLLADO POR STRAiKU</p>
     </div>
   )
 }
--- src/layouts/LandingPageLayout/components/Footer/Footer.tsx
+++ src/layouts/LandingPageLayout/components/Footer/Footer.tsx
@@ -3,13 +3,13 @@ import WaterMark from '../../../../components/WaterMark/WaterMark'
 
 export default function Footer() {
   return (
-    <footer className="py-20">
+    <footer className="mx-auto max-w-7xl pt-20 pb-12 max-[850px]:pt-12">
       <div className="flex justify-around gap-2 px-6 max-[850px]:flex-wrap">
         <div className="w-96 flex-col justify-start gap-5 max-[850px]:mx-2 max-[850px]:w-full">
-          <h6 className="font-secondary text-left text-2xl italic max-[850px]:text-lg">
+          <h6 className="font-secondary text-left text-2xl italic max-[850px]:text-center max-[850px]:text-xl">
             WishFactory
           </h6>
-          <p className="text-sm">
+          <p className="text-sm max-[850px]:mt-2 max-[850px]:text-center max-[850px]:text-xs">
             Un experimento en intención digital y arquitectura del deseo. Propiedad intelectual de
             los alquimistas.
           </p>
@@ -17,17 +17,25 @@ export default function Footer() {
         <div className="flex flex-row justify-around gap-20 max-[850px]:mt-5 max-[850px]:gap-5">
           <div className="font-primary flex flex-col flex-wrap justify-between gap-3 text-sm">
             <p className="text-contrast mb-3">PLATAFORMA</p>
-            <Link to="/">WishFactory</Link>
-            <Link to="/mixwish">MixWish</Link>
-            <Link to="/terms">Términos de uso y condiciones</Link>
+            <Link className="max-[850px]:text-xs" to="/">
+              WishFactory
+            </Link>
+            <Link className="max-[850px]:text-xs" to="/mixwish">
+              MixWish
+            </Link>
+            <Link className="max-[850px]:text-xs" to="/terms">
+              Términos de uso y condiciones
+            </Link>
           </div>
           <div className="flex flex-col flex-wrap justify-between gap-3 text-sm">
             <p className="text-contrast mb-3">CONECTAR</p>
-            <a href="https://instagram.com/straiku" target="_blank">
+            <a className="max-[850px]:text-xs" href="https://instagram.com/straiku" target="_blank">
               Instagram
             </a>
-            <Link to="mailto:straiku.official@gmail.com">Correo</Link>
-            <a href="https://github.com/straiku" target="_blank">
+            <Link className="max-[850px]:text-xs" to="mailto:straiku.official@gmail.com">
+              Correo
+            </Link>
+            <a className="max-[850px]:text-xs" href="https://github.com/straiku" target="_blank">
               Github - STRAiKU
             </a>
           </div>
