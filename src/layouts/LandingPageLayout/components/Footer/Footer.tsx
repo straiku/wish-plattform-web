09b4683f353d94eb6250592d91fa16123f24204b
JustEMAKILLER <emanuelnuviola@gmail.com>
2026-05-09 21:22:23 -0400

    style(Footer): minor fixes

--- src/layouts/LandingPageLayout/components/Footer/Footer.tsx
+++ src/layouts/LandingPageLayout/components/Footer/Footer.tsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom'
 
 export default function Footer() {
   return (
-    <footer>
-      <div className="flex justify-around gap-2 px-6">
-        <div className="w-96 flex-col justify-start gap-5 max-[850px]:mx-2">
-          <h6 className="font-secondary text-2xl italic max-[850px]:ml-6 max-[850px]:text-lg">
+    <footer className="py-20">
+      <div className="flex justify-around gap-2 px-6 max-[850px]:flex-wrap">
+        <div className="w-96 flex-col justify-start gap-5 max-[850px]:mx-2 max-[850px]:w-full">
+          <h6 className="font-secondary text-left text-2xl italic max-[850px]:text-lg">
             WishFactory
           </h6>
           <p className="text-sm">
@@ -13,7 +13,7 @@ export default function Footer() {
             los alquimistas.
           </p>
         </div>
-        <div className="flex flex-row justify-around gap-20">
+        <div className="flex flex-row justify-around gap-20 max-[850px]:mt-5 max-[850px]:gap-5">
           <div className="font-primary flex flex-col flex-wrap justify-between gap-3 text-sm">
             <p className="text-secondary mb-3">PLATAFORMA</p>
             <Link to="/">WishFactory</Link>
