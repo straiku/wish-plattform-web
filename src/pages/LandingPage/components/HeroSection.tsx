16843f4d03003e51edd3e03f2f7a5846b9159033
lianni <liadev2006@gmail.com>
2026-05-11 16:04:57 -0500

    fix(LandingPage): rename LandingPageComponents

--- src/pages/LandingPage/components/HeroSection.tsx
+++ src/pages/LandingPage/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@heroui/react'
 
-export default function Hero() {
+export default function HeroSection() {
   return (
     <section className="flex min-h-screen max-w-7xl items-center px-8 py-32 max-[1280px]:mx-6 max-[1280px]:px-0 min-[1280px]:mx-auto">
       <div className="flex flex-col gap-4 text-left">
