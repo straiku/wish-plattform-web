2730c97dcaeee4e36cf58e0dfadb2c322e7628bd
lianni <liadev2006@gmail.com>
2026-05-23 15:31:38 -0500

    style(WishCard): replace single quotes with double quotes

--- src/pages/LandingPage/components/RemixesSection.tsx
+++ src/pages/LandingPage/components/RemixesSection.tsx
@@ -3,7 +3,7 @@ import HowItWorks from './HowItWorks'
 
 export default function RemixesSection() {
   return (
-    <section  id='remixes' className="bg-primary mx-auto flex h-auto max-w-7xl flex-wrap justify-between px-6 py-36 max-[970px]:px-3 max-[970px]:py-14 max-[870px]:justify-center">
+    <section id="remixes" className="bg-primary mx-auto flex h-auto max-w-7xl flex-wrap justify-between px-6 py-36 max-[970px]:px-3 max-[970px]:py-14 max-[870px]:justify-center">
       <HeroRemixes />
       <HowItWorks />
     </section>
