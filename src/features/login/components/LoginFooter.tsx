9c39f464e101e0f31a7b85d86281b047bf12b156
unknown <liadev2006@gmail.com>
2026-07-02 14:20:53 -0500

    feat(LoginFooter): make LoginFooter responsive

--- src/features/login/components/LoginFooter.tsx
+++ src/features/login/components/LoginFooter.tsx
@@ -5,22 +5,22 @@ export default function LoginFooter(){
 
   return(
 
-    <div className="w-full flex flex-wrap text-zinc-400  justify-between items-center h-[20vh] px-15 bg-primary max-[850px]:px-5 max-[850px]:gap-8 ">
-      <p className=" text-[1.35rem] italic font-secondary ">WishFactory</p>
+    <footer className="w-full flex flex-wrap text-zinc-400 justify-between items-center h-auto px-15 py-12 bg-primary max-[350px]:justify-center max-[550px]:gap-0.5 max-[550px]:py-4 max-[550px]:px-4 max-[850px]:gap-4 ">
+      <p className="text-[1.35rem] italic font-secondary max-[600px]:w-full max-[600px]:text-center ">WishFactory</p>
     
-      <div className="w-auto gap-10 text-[0.75rem]  flex tracking-widest justify-between items-center font-light max-[850px]:w-auto max-[900px]:gap-4">
-        <Link  className="max-[850px]:text-xs" href="/">
+      <div className="w-auto flex justify-between gap-13 text-[0.75rem] tracking-widest font-light h-min  max-[550px]:gap-1  max-[850px]:text-[0.5rem] max-[850px]:gap-10   ">
+        <Link href="/">
           PRIVACIDAD
         </Link>
-        <Link className="max-[850px]:text-xs" href="/">
+        <Link href="/">
           TÉRMINOS
         </Link>
-        <a className="max-[850px]:text-xs" href="mailto:straiku.official@gmail.com">
-        CONTACTO
+        <a href="mailto:straiku.official@gmail.com">
+          CONTACTO
         </a>
       </div>
 
-      <p className="text-[0.75rem] block  tracking-widest">@ 2026 THE DIGITAL ALCHEMIST</p>
-  </div>
+      <p className="text-[0.75rem] w-auto tracking-widest max-[850px]:text-[0.5rem]">@ 2026 THE DIGITAL ALCHEMIST</p>
+  </footer>
   )
 }
